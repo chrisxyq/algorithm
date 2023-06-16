9c31c3bd4460bdc98365aa94fe883ba5bc613a0e
chrisxyq <yuanqixu@trip.com>
2023-06-16 18:05:06 +0800

    AvoidRain

---
+++ contest-avoiding-rain/src/main/java/com/chrisxyq/contestavoidingrain/ContestAvoidingRainApplication.java
@@ -0,0 +1,13 @@
+package com.chrisxyq.contestavoidingrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContestAvoidingRainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContestAvoidingRainApplication.class, args);
+    }
+
+}
---
+++ contest-avoiding-rain/src/test/java/com/chrisxyq/contestavoidingrain/ContestAvoidingRainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrisxyq.contestavoidingrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContestAvoidingRainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
